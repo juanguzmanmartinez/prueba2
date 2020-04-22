463df427efcb62374d2de966eecdb6590e0866d5
balocenm <balocenm@gmail.com>
2020-04-22 14:55:43 -0500

    feat(OM-782): Config dependences in the project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./business/business.module').then(m => m.BusinessModule),
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
