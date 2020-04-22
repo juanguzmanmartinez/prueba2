e99f9289008671f20e569283065433120882fef4
balocenm <balocenm@gmail.com>
2020-04-22 14:58:49 -0500

    feat(OM-782): file organization

---
+++ src/app/business/business.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BusinessComponent } from './business.component';
+import { BusinessRoutingModule } from './business-routing.module';
+
+
+
+@NgModule({
+  declarations: [BusinessComponent],
+  imports: [
+    CommonModule,
+    BusinessRoutingModule,
+  ]
+})
+export class BusinessModule { }
