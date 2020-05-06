31a0342c391f3c0b64ff79b63f35b975095fe6b0
balocenm <balocenm@gmail.com>
2020-05-06 14:53:36 -0500

    feat(OM-742):  delete declaration corecommonsmodule

--- src/app/commons/commons.module.ts
+++ src/app/commons/commons.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CoreComponentsModule } from './core-components/core-components.module';
 
 
 
 @NgModule({
-  declarations: [CoreComponentsModule],
+  declarations: [],
   imports: [
     CommonModule
   ]
