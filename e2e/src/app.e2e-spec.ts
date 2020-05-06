36d79a101b76cfeb40011267f83bad57c1c6056b
balocenm <balocenm@gmail.com>
2020-05-06 14:11:41 -0500

    feat(OM-782): name project change config

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('WEB-Inka-Dashboard app is running!');
+    expect(page.getTitleText()).toEqual('WEB-admin-operaciones app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'WEB-Inka-Dashboard'`, () => {
+  it(`should have as title 'WEB-admin-operaciones'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('WEB-Inka-Dashboard');
+    expect(app.title).toEqual('WEB-admin-operaciones');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('WEB-Inka-Dashboard app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('WEB-admin-operaciones app is running!');
   });
 });
