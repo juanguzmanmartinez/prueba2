5f0e8a169b296b76b8042171321b4d7e341ca1f7
balocenm <balocenm@gmail.com>
2020-05-26 22:01:31 -0500

    fix(OM-846): update enpoint

--- src/app/shared/parameters/endpoints.ts
+++ src/app/shared/parameters/endpoints.ts
@@ -5,5 +5,5 @@ export class ENDPOINTS {
   public static GET_CALENDAR = `${environment.api_gateway_calendar}/schedule/calendar`;
   public static PATCH_CALENDAR = `${environment.api_gateway_calendar}/schedule/calendar/days`;
   public static GET_BLOCKSCHEDULE = `${environment.api_gateway_calendar}/schedule/calendar/detail`;
-  public static PATCH_CAPACITY = `${environment.api_gateway_calendar}/schedule/calendar/detail/hours/`;
+  public static PATCH_CAPACITY = `${environment.api_gateway_calendar}/schedule/calendar/detail/hours/quantities`;
 }
