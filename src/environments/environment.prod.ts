64af03a27cfd4af12060c4f17cf21d05aeb5de79
balocenm <balocenm@gmail.com>
2020-05-29 09:47:42 -0500

    fix(OM-853): reconfig enviroment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   show_logs: false,
   envName: 'prod',
-  api_gateway: 'https://radxzrb1ok.execute-api.us-east-1.amazonaws.com/CI02/api/v1.0/fulfillment',
-  api_gateway_calendar: 'https://r7540g00k4.execute-api.us-east-1.amazonaws.com/CI02/api/v2.5/fulfillment',
+  api_gateway: 'https://radxzrb1ok.execute-api.us-east-1.amazonaws.com/PROD/api/v1.0/fulfillment',
+  api_gateway_calendar: 'https://r7540g00k4.execute-api.us-east-1.amazonaws.com/PROD/api/v2.5/fulfillment',
 };
