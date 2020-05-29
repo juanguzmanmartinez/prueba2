1ac2f3aff56cb01e60dc798eb1c9adebd4d7026a
balocenm <balocenm@gmail.com>
2020-05-29 08:32:15 -0500

    fix(OM-853): config enviroment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  show_logs: false,
+  envName: 'prod',
+  api_gateway: 'https://radxzrb1ok.execute-api.us-east-1.amazonaws.com/PROD/api/v1.0/fulfillment',
+  api_gateway_calendar: 'https://r7540g00k4.execute-api.us-east-1.amazonaws.com/PROD/api/v2.5/fulfillment',
 };
