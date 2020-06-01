5f259e71559f4aa9338e4c4c6d9d7e81928fc8b8
gemerson <henryingtel@gmail.com>
2020-06-01 13:54:24 -0500

    feat(ON-747): updating config for deploy environment

--- devops/setup.sh
+++ devops/setup.sh
@@ -11,15 +11,12 @@ elif [ "$environment" == "ci2" ]
 then
     echo "Setting up project $app:$environment"
 
-
     echo "Starting Web Server ..."
     nginx -g 'daemon off;'
 
 elif [ "$environment" == "qa" ]
 then
     echo "Setting up project $app:$environment"
-
-
 
     echo "CONTENTFUL CONFIG Settings for CIQA enviroment"
     sed -i -e 's/environment:"develop"/environment:"UAT"/g' /usr/share/nginx/html/main*.js
@@ -32,8 +29,8 @@ elif [ "$environment" == "qa2" ]
 then
     echo "Setting up project $app:$environment"
 
-
-
+    sed -i -e 's/https:\/\/radxzrb1ok.execute-api.us-east-1.amazonaws.com\/CI02/https:\/\/radxzrb1ok.execute-api.us-east-1.amazonaws.com\/QA02/g' /usr/share/nginx/html/main*.js
+    sed -i -e 's/https:\/\/r7540g00k4.execute-api.us-east-1.amazonaws.com\/CI02/https:\/\/r7540g00k4.execute-api.us-east-1.amazonaws.com\/QA02/g' /usr/share/nginx/html/main*.js
 
     echo "CONTENTFUL CONFIG Settings for CIQA enviroment"
     sed -i -e 's/environment:"develop"/environment:"UAT"/g' /usr/share/nginx/html/main*.js
@@ -42,24 +39,26 @@ then
     echo "Starting Web Server ..."
     nginx -g 'daemon off;'
 
-  elif [ "$environment" == "uat" ]
-  then
-      echo "Setting up project $app:$environment"
+elif [ "$environment" == "uat" ]
+then
+    echo "Setting up project $app:$environment"
 
+    sed -i -e 's/https:\/\/radxzrb1ok.execute-api.us-east-1.amazonaws.com\/CI02/https:\/\/radxzrb1ok.execute-api.us-east-1.amazonaws.com\/UAT/g' /usr/share/nginx/html/main*.js
+    sed -i -e 's/https:\/\/r7540g00k4.execute-api.us-east-1.amazonaws.com\/CI02/https:\/\/r7540g00k4.execute-api.us-east-1.amazonaws.com\/UAT/g' /usr/share/nginx/html/main*.js
 
-
-      echo "Starting Web Server"
-      nginx -g 'daemon off;'
+    echo "Starting Web Server"
+    nginx -g 'daemon off;'
 
 elif [ "$environment" == "prd" ]
 then
 
     echo "Setting up project $app:$environment"
 
+    sed -i -e 's/https:\/\/radxzrb1ok.execute-api.us-east-1.amazonaws.com\/CI02/https:\/\/radxzrb1ok.execute-api.us-east-1.amazonaws.com\/PRD/g' /usr/share/nginx/html/main*.js
+    sed -i -e 's/https:\/\/r7540g00k4.execute-api.us-east-1.amazonaws.com\/CI02/https:\/\/r7540g00k4.execute-api.us-east-1.amazonaws.com\/PRD/g' /usr/share/nginx/html/main*.js
 
-
-      echo "Starting Web Server"
-      nginx -g 'daemon off;'
+    echo "Starting Web Server"
+    nginx -g 'daemon off;'
 else
     echo "Please enter a valid environment option [ ci, ci2, qa, qa2, prod]"
 fi
