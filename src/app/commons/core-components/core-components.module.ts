375c04911006ea2f1ccb7682784f19b412a17256
katherineortega <katy.ja2915@gmail.com>
2020-10-07 18:32:00 -0500

    feat(OM-944) add components to capacity

--- src/app/commons/core-components/core-components.module.ts
+++ src/app/commons/core-components/core-components.module.ts
@@ -4,16 +4,12 @@ import { DropdownComponent } from './dropdown/dropdown.component';
 import { FormsModule } from '@angular/forms';
 import { InputTextCustomerComponent } from './input-text-customer/input-text-customer.component';
 import { InputNumberGenericDirective } from './input-text-customer/input-number.directive';
-import { RadioControlComponent } from './radio-control/radio-control.component';
-import { TooltipComponent } from './tooltip/tooltip.component';
 import {MatTooltipModule} from '@angular/material/tooltip';
 
 const COMPONENTES = [
   DropdownComponent,
   InputTextCustomerComponent,
   InputNumberGenericDirective,
-  RadioControlComponent,
-  TooltipComponent,
 ];
 
 @NgModule({
