1022b2243ced9f2bc99029c3f130eefeefbd1339
katherineortega <katy.ja2915@gmail.com>
2021-02-04 10:38:38 -0500

    feat(OM-1021) update apigateway

--- src/app/shared/parameters/endpoints.parameter.ts
+++ src/app/shared/parameters/endpoints.parameter.ts
@@ -1,12 +1,7 @@
 import { environment } from '@environments/environment';
 
-export enum EApiVersion {
-  api_v1_0 = '/api/v1.0',
-  api_v2_0 = '/api/v2.0'
-}
-
-const API_GATEWAY = `${environment.api_gateway}${EApiVersion.api_v1_0}`;
-const API_GATEWAY_CALENDAR = `${environment.api_gateway_calendar}${EApiVersion.api_v2_0}`;
+const API_GATEWAY = `${environment.api_gateway}`;
+const API_GATEWAY_CALENDAR = `${environment.api_gateway_calendar}`;
 const API_GATEWAY_AUTH = `${environment.api_gateway_auth}`;
 
 export class EndpointsParameter {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   show_logs: false,
   envName: 'dev',
-  api_gateway: 'https://radxzrb1ok.execute-api.us-east-1.amazonaws.com/CI02',
-  api_gateway_calendar: 'https://r7540g00k4.execute-api.us-east-1.amazonaws.com/CI02',
+  api_gateway: 'https://radxzrb1ok.execute-api.us-east-1.amazonaws.com/CI02/api/v1.0',
+  api_gateway_calendar: 'https://r7540g00k4.execute-api.us-east-1.amazonaws.com/CI02/api/v2.0',
   api_gateway_auth: 'https://28c9y4dvs8.execute-api.us-east-1.amazonaws.com/CI02',
 };
