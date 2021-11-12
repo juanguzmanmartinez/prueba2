566319ea85dc56f9576f617a57119f0d87f3729a
Alejandro Rodriguez Romero <alex98rodriguezromero@gmail.com>
2021-11-12 13:44:14 -0500

    feat(feature-32845): Base components are modified

--- src/app/core/molecules/table/directives/table-header-row.directive.ts
+++ src/app/core/molecules/table/directives/table-header-row.directive.ts
@@ -1,18 +1,24 @@
-import { AfterViewInit, Directive, ElementRef, Renderer2 } from '@angular/core';
+import { AfterViewInit, Directive, ElementRef, Input, Renderer2 } from '@angular/core';
 
 @Directive({
-    selector: 'tr[mat-header-row]'
+  selector: 'tr[mat-header-row]'
 })
 export class TableHeaderRowDirective implements AfterViewInit {
 
-    constructor(
-        private elementRef: ElementRef,
-        private renderer: Renderer2
-    ) {
-    }
+  private color = 'bg-gray-5';
 
-    ngAfterViewInit() {
-        this.renderer.addClass(this.elementRef.nativeElement, 'bg-gray-5');
-        this.renderer.setStyle(this.elementRef.nativeElement, 'height', '50px');
-    }
+  @Input('class-color')
+  set classColor(color: string) {
+    this.color = color;
+  }
+
+  constructor(
+    private elementRef: ElementRef,
+    private renderer: Renderer2
+  ) { }
+
+  ngAfterViewInit() {
+    this.renderer.addClass(this.elementRef.nativeElement, this.color);
+    this.renderer.setStyle(this.elementRef.nativeElement, 'height', '48px');
+  }
 }
