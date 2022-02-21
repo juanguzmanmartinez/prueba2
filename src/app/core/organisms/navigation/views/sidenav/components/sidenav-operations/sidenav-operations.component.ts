4a2e8e5911617e906eb8a6de21eb2f3917d866a4
Emil Helmut Zapata Arellano <EmilZapata95@gmail.com>
2022-02-21 16:14:35 -0500

    refacotor(feature-order): refactor column PromiseDate, focus in Select component and refactor Sidenav

--- src/app/core/organisms/navigation/views/sidenav/components/sidenav-operations/sidenav-operations.component.ts
+++ src/app/core/organisms/navigation/views/sidenav/components/sidenav-operations/sidenav-operations.component.ts
@@ -1,23 +1,24 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { OPERATIONS_ROUTER } from '@parameters/router/routing/operations/operations-router.parameter';
-import { ROUTER_PATH } from '@parameters/router/router-path.parameter';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
+import { ROUTER_PATH } from '@parameters/router/router-path.parameter';
+import { OPERATIONS_ROUTER } from '@parameters/router/routing/operations/operations-router.parameter';
 
 @Component({
-    selector: 'app-sidenav-operations',
-    templateUrl: './sidenav-operations.component.html',
-    styleUrls: ['./sidenav-operations.component.sass']
+  selector: 'app-sidenav-operations',
+  templateUrl: './sidenav-operations.component.html',
+  styleUrls: ['./sidenav-operations.component.sass'],
 })
 export class SidenavOperationsComponent implements OnInit {
+  public routerPath = ROUTER_PATH;
+  public sidenavRouting = OPERATIONS_ROUTER;
+  @Input() sidenav: MatSidenav;
+  @Output() sidenavOpen = new EventEmitter();
 
-    public routerPath = ROUTER_PATH;
-    public sidenavRouting = OPERATIONS_ROUTER;
-    @Input() sidenav: MatSidenav;
+  constructor() {}
 
-    constructor() {
-    }
+  ngOnInit(): void {}
 
-    ngOnInit(): void {
-    }
-
+  onSidenavOpen() {
+    this.sidenavOpen?.emit();
+  }
 }
--- src/app/core/organisms/navigation/views/sidenav/sidenav.component.ts
+++ src/app/core/organisms/navigation/views/sidenav/sidenav.component.ts
@@ -1,57 +1,57 @@
-import { Component, OnDestroy, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
-import { NavigationService } from '@organisms/navigation/navigation.service';
+import {
+  Component,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+  ViewEncapsulation,
+} from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
+import { NavigationService } from '@organisms/navigation/navigation.service';
 import { Subscription } from 'rxjs';
 
 @Component({
-    selector: 'app-sidenav',
-    templateUrl: './sidenav.component.html',
-    styleUrls: ['./sidenav.component.sass'],
-    encapsulation: ViewEncapsulation.None
+  selector: 'app-sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrls: ['./sidenav.component.sass'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class SidenavComponent implements OnInit, OnDestroy {
-    @ViewChild(MatSidenav, {static: true}) sidenav: MatSidenav;
-    private subscriptions: Subscription[] = [];
+  @ViewChild(MatSidenav, { static: true }) sidenav: MatSidenav;
+  private subscriptions: Subscription[] = [];
 
-    constructor(
-        private navigationService: NavigationService
-    ) {
-    }
+  constructor(private navigationService: NavigationService) {}
 
-    ngOnInit(): void {
-        this.openSidenavEvent();
-        this.closeSidenavEvent();
-    }
+  ngOnInit(): void {
+    this.openSidenavEvent();
+    this.closeSidenavEvent();
+  }
 
-    openSidenavEvent() {
-        const subscription = this.navigationService.openSidenav$
-            .subscribe(() => {
-                this.sidenav.open();
-            });
+  openSidenavEvent() {
+    const subscription = this.navigationService.openSidenav$.subscribe(() => {
+      this.sidenav.open();
+    });
 
-        this.subscriptions.push(subscription);
-    }
+    this.subscriptions.push(subscription);
+  }
 
-    closeSidenavEvent() {
-        const subscription = this.navigationService.closeSidenav$
-            .subscribe(() => {
-                this.sidenav.close();
-            });
+  closeSidenavEvent() {
+    const subscription = this.navigationService.closeSidenav$.subscribe(() => {
+      this.sidenav.close();
+    });
 
-        this.subscriptions.push(subscription);
-    }
+    this.subscriptions.push(subscription);
+  }
 
-    sidenavOpened() {
-        this.navigationService.sidenavOpened = true;
-    }
+  sidenavOpened() {
+    this.navigationService.sidenavOpened = true;
+    this.sidenav.open();
+  }
 
-    sidenavClosed() {
-        this.navigationService.sidenavClosed = true;
-    }
+  sidenavClosed() {
+    this.navigationService.sidenavClosed = true;
+  }
 
-
-    ngOnDestroy() {
-        this.subscriptions.forEach(subscription => subscription.unsubscribe());
-    }
-
+  ngOnDestroy() {
+    this.subscriptions.forEach((subscription) => subscription.unsubscribe());
+  }
 }
