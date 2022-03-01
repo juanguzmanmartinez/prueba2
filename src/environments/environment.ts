dcc747c4d109dfa64d21a4c7ddca5a67e8dd4baf
Emil Helmut Zapata Arellano <EmilZapata95@gmail.com>
2022-03-01 10:17:54 -0500

    refactor:(feature-order): return enviroments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,25 +2,14 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-// export const environment = {
-//   production: false,
-//   show_logs: false,
-//   envName: 'dev',
-//   api_gateway: 'https://4kz2r4t838.execute-api.us-east-1.amazonaws.com/CI02',
-//   api_gateway_order: 'https://4kz2r4t838.execute-api.us-east-1.amazonaws.com/CI02',
-//   api_gateway_auth: 'https://28c9y4dvs8.execute-api.us-east-1.amazonaws.com/CI02',
-//   authUsername: 'adm-operations',
-//   authPassword: '12345',
-//   dataStudio: 'https://datastudio.google.com/embed/reporting/6ac0e5bb-786a-4371-98f2-5a2e8035d871',
-// };
 export const environment = {
   production: false,
   show_logs: false,
-  envName: 'qa2',
-  api_gateway: 'https://4kz2r4t838.execute-api.us-east-1.amazonaws.com/QA02',
-  api_gateway_order: 'https://4kz2r4t838.execute-api.us-east-1.amazonaws.com/QA02',
-  api_gateway_auth: 'https://28c9y4dvs8.execute-api.us-east-1.amazonaws.com/QA02',
+  envName: 'dev',
+  api_gateway: 'https://4kz2r4t838.execute-api.us-east-1.amazonaws.com/CI02',
+  api_gateway_order: 'https://4kz2r4t838.execute-api.us-east-1.amazonaws.com/CI02',
+  api_gateway_auth: 'https://28c9y4dvs8.execute-api.us-east-1.amazonaws.com/CI02',
   authUsername: 'adm-operations',
-  authPassword: 'ypej47Qm14QhjB93Sf',
-  dataStudio: 'https://datastudio.google.com/embed/reporting/6ac0e5bb-786a-4371-98f2-5a2e8035d871'
+  authPassword: '12345',
+  dataStudio: 'https://datastudio.google.com/embed/reporting/6ac0e5bb-786a-4371-98f2-5a2e8035d871',
 };
