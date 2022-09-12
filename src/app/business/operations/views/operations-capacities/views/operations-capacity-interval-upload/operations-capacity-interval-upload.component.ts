93f227a4a39e92ddddc11cf584ca22139dd0a38a
Juan Guzman <soluciondigital16@farmaciasperuanas.pe>
2022-09-11 20:57:56 -0500

    feat:agregar carga de intervaltime

--- src/app/business/operations/views/operations-capacities/views/operations-capacity-interval-upload/operations-capacity-interval-upload.component.ts
+++ src/app/business/operations/views/operations-capacities/views/operations-capacity-interval-upload/operations-capacity-interval-upload.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+// import { TABS } from '../../constants/step-tabs.constants';
+// import { UploadCapacitiesStoreService } from '../../stores/upload-capacities-store.service';
 @Component({
   selector: 'app-operations-capacity-interval-upload',
   templateUrl: './operations-capacity-interval-upload.component.html',
-  styleUrls: ['./operations-capacity-interval-upload.component.sass']
+  styleUrls: ['./operations-capacity-interval-upload.component.scss'],
 })
 export class OperationsCapacityIntervalUploadComponent implements OnInit {
-
-  constructor() { }
+  // private subscriptions = new Subscription();
+  // stepTabs = [];
+  // currentStep: string = '1';
+  // constructor(
+  //   private _uploadCapacitiesStoreService: UploadCapacitiesStoreService
+  // ) {}
 
   ngOnInit(): void {
+    // // this.currentStep = this._uploadCapacitiesStoreService.getCurrentStep();
+    // const subscription =
+    //   this._uploadCapacitiesStoreService.getCurrentStep$.subscribe(
+    //     (eCapacityStepStatus: any) => {
+    //       this.currentStep = eCapacityStepStatus;
+    //     }
+    //   );
+    // this.subscriptions.add(subscription);
+    // const subscription1 = this._uploadCapacitiesStoreService.setStepsTabs(TABS);
+    // this._uploadCapacitiesStoreService.getStepsTabs$.subscribe((stepsTabs) => {
+    //   this.stepTabs = stepsTabs;
+    // });
+    // this.subscriptions.add(subscription1);
   }
-
+  // ngOnDestroy(): void {
+  //   this.subscriptions.unsubscribe();
+  // }
 }
