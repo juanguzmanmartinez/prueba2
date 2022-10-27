4d9081c0f30d3e5d14c7a3fdeb19fad0e8c0bb57
= <soluciondigital73@farmaciasperuanas.pe>
2022-10-27 09:14:38 -0500

    added order pagination interface

--- src/app/shared/stores/order-filter-store.service.ts
+++ src/app/shared/stores/order-filter-store.service.ts
@@ -1,14 +1,21 @@
 import { Injectable } from '@angular/core';
 import { IDrugstore } from '@interfaces/drugstores/drugstores.interface';
-import { IOrderFilter } from 'app/business/order/views/order-records/interfaces/order-filter.interface';
+import {
+  IOrderFilter,
+  IOrderPagination,
+} from 'app/business/order/views/order-records/interfaces/order-filter.interface';
 import { BehaviorSubject } from 'rxjs';
 
 @Injectable()
 export class OrderFilterStore {
   private orderFilter: IOrderFilter = {};
+  private orderPaginationInit: IOrderPagination = { page: 1, pageSize: 10 };
   private orderFilterSubject = new BehaviorSubject<IOrderFilter>({});
   private localList = new BehaviorSubject<IDrugstore[]>([]);
   private isResetFilters = new BehaviorSubject<boolean>(false);
+  private orderPaginationSubject = new BehaviorSubject<IOrderPagination>(
+    this.orderPaginationInit
+  );
 
   public orderFilter$ = this.orderFilterSubject.asObservable();
 
@@ -36,6 +43,20 @@ export class OrderFilterStore {
 
   setIsResetFilters(isReset: boolean) {
     this.isResetFilters.next(isReset);
+  }
+
+  setPage(page: number) {
+    const orderPagination = { ...this.orderPaginationSubject.value, page };
+    this.orderPaginationSubject.next(orderPagination);
+  }
+
+  setPageSize(pageSize: number) {
+    const orderPagination = { ...this.orderPaginationSubject.value, pageSize };
+    this.orderPaginationSubject.next(orderPagination);
+  }
+
+  getOrderPagination() {
+    return this.orderPaginationSubject.asObservable();
   }
 
   set setSearchCode(searchCode: string) {
