75a5d312df8bf9f89b9a77acf1348e662c95d713
Victor Morales <victor.morales-HENKCORP@farmaciasperuanas.pes>
2023-02-22 14:47:59 -0500

    fix: disable button

--- src/app/shared/helpers/disable-cancel-order.helper.ts
+++ src/app/shared/helpers/disable-cancel-order.helper.ts
@@ -11,5 +11,6 @@ export const LStatusNotAllowed:string[] = [
   EStatusOrder.cancelled,
   EStatusOrder.onRouted,
   EStatusOrder.arrived,
-  EStatusOrder.delivered
+  EStatusOrder.delivered,
+  EStatusOrder.rejected
 ]
