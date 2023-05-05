e2fabbd1f72eb90a8958a8e502c3c3e0ae7c73a1
Juan Guzman <soluciondigital16@farmaciasperuanas.pe>
2023-05-04 19:24:49 -0500

    feat:router-tracking view

--- src/app/business/control-tower/views/route-monitoring/route-monitoring.module.ts
+++ src/app/business/control-tower/views/route-monitoring/route-monitoring.module.ts
@@ -4,6 +4,16 @@ import { RouteMonirotingRoutingModule } from './route-monitoring-routing.module'
 import { AllocationRoutingComponent } from './views/allocation-routing/allocation-routing.component';
 import { ManualRoutingComponent } from './views/manual-routing/manual-routing.component';
 import { RouteTrackingComponent } from './views/route-tracking/route-tracking.component';
+import { BackRouterModule } from '@molecules/back-router/back-router.module';
+import { BackRouterSimpleModule } from '@molecules/back-router-simple/back-router-simple.module';
+import { ButtonsModule } from '@atoms/buttons/buttons.module';
+import { IconsModule } from '@atoms/icons/icons.module';
+import { SelectModule } from '@atoms/select/select.module';
+import { TableModule } from '@molecules/table/table.module';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { DirectivesModule } from 'app/shared/directives/directives.module';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [
@@ -12,6 +22,15 @@ import { RouteTrackingComponent } from './views/route-tracking/route-tracking.co
     AllocationRoutingComponent,
     ManualRoutingComponent,
   ],
-  imports: [RouteMonirotingRoutingModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    RouteMonirotingRoutingModule,
+    BackRouterModule,
+    BackRouterSimpleModule,
+    ButtonsModule,
+    IconsModule,
+    SelectModule,TableModule,DirectivesModule,MatTableModule
+  ],
 })
 export class RouteMonitoringModule {}
--- src/app/business/control-tower/views/route-monitoring/views/route-tracking/route-tracking.component.ts
+++ src/app/business/control-tower/views/route-monitoring/views/route-tracking/route-tracking.component.ts
@@ -1,7 +1,133 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-route-tracking',
   templateUrl: './route-tracking.component.html',
+  styleUrls:['./route-tracking.component.scss']
 })
-export class RouteTrackingComponent {}
+export class RouteTrackingComponent implements OnInit {
+
+  selectedCompanies = [];
+  selectedLocals = [];
+  companies = ['DISPONIBLE', 'EN RUTA', 'NO DISPONIBLE'];
+  displayedColumns: string[] = [
+    'local',
+    'carrier',
+    'provider',
+    'vehicleType',
+    'startHour',
+    'status',
+    'paused',
+    'actions',
+  ];
+  dataSource = new MatTableDataSource<any>();
+  dataFake: any[] = [
+    {
+      idCarrier: '1',
+      local: 'IKF-061-DOS DE MAYO',
+      carrier: 'Renato Fernandez',
+      provider: 'CAJE FOREST',
+      vehicleType: 'Moto',
+      startHour: '1:02 p.m.',
+      status: 'DISPONIBLE',
+      paused: 'No',
+    },
+    {
+      idCarrier: '2',
+      local: 'IKF-061-DOS DE MAYO',
+      carrier: 'Renato Fernandez',
+      provider: 'CAJE FOREST',
+      vehicleType: 'Moto',
+      startHour: '1:02 p.m.',
+      status: 'EN RUTA',
+      paused: 'No',
+    },
+    {
+      idCarrier: '3',
+      local: 'IKF-061-DOS DE MAYO',
+      carrier: 'Renato Fernandez',
+      provider: 'CAJE FOREST',
+      vehicleType: 'Moto',
+      startHour: '1:02 p.m.',
+      status: 'NO DISPONIBLE',
+      paused: 'No',
+    },
+    {
+      idCarrier: '4',
+      local: 'IKF-061-DOS DE MAYO',
+      carrier: 'Renato Fernandez',
+      provider: 'CAJE FOREST',
+      vehicleType: 'Moto',
+      startHour: '1:02 p.m.',
+      status: 'DISPONIBLE',
+      paused: 'No',
+    },
+    {
+      idCarrier: '5',
+      local: 'IKF-061-DOS DE MAYO',
+      carrier: 'Renato Fernandez',
+      provider: 'CAJE FOREST',
+      vehicleType: 'Moto',
+      startHour: '1:02 p.m.',
+      status: 'DISPONIBLE',
+      paused: 'No',
+    },
+    {
+      idCarrier: '6',
+      local: 'IKF-061-DOS DE MAYO',
+      carrier: 'Renato Fernandez',
+      provider: 'CAJE FOREST',
+      vehicleType: 'Moto',
+      startHour: '1:02 p.m.',
+      status: 'DISPONIBLE',
+      paused: 'No',
+    },
+    {
+      idCarrier: '7',
+      local: 'IKF-061-DOS DE MAYO',
+      carrier: 'Renato Fernandez',
+      provider: 'CAJE FOREST',
+      vehicleType: 'Moto',
+      startHour: '1:02 p.m.',
+      status: 'DISPONIBLE',
+      paused: 'No',
+    },
+    {
+      idCarrier: '8',
+      local: 'IKF-061-DOS DE MAYO',
+      carrier: 'Renato Fernandez',
+      provider: 'CAJE FOREST',
+      vehicleType: 'Moto',
+      startHour: '1:02 p.m.',
+      status: 'DISPONIBLE',
+      paused: 'No',
+    },
+    {
+      idCarrier: '9',
+      local: 'IKF-061-DOS DE MAYO',
+      carrier: 'Renato Fernandez',
+      provider: 'CAJE FOREST',
+      vehicleType: 'Moto',
+      startHour: '1:02 p.m.',
+      status: 'DISPONIBLE',
+      paused: 'No',
+    },
+    {
+      idCarrier: '10',
+      local: 'IKF-061-DOS DE MAYO',
+      carrier: 'Renato Fernandez',
+      provider: 'CAJE FOREST',
+      vehicleType: 'Moto',
+      startHour: '1:02 p.m.',
+      status: 'DISPONIBLE',
+      paused: 'No',
+    },
+  ];
+ngOnInit(): void {
+    this.dataSource.data =this.dataFake
+}
+  selectionChange(e: any) {}
+  clearValues() {}
+  viewCarrierRoute(id){}
+}
