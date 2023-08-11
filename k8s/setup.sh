d722a34699597b4b81d45db0795080c0154a746e
= <soluciondigital73@farmaciasperuanas.pe>
2023-08-11 10:31:00 -0500

    added dev and qas config in setup.sh

--- k8s/setup.sh
+++ k8s/setup.sh
@@ -13,6 +13,17 @@ then
     echo "Starting Web Server ..."
     nginx -g 'daemon off;'
 
+elif [ "$environment" == "dev" ]
+then
+    echo "Setting up project $app:$environment"
+
+    sed -i -e 's/https:\/\/4kz2r4t838.execute-api.us-east-1.amazonaws.com\/CI02/https:\/\/fduetuxpf0.execute-api.us-east-1.amazonaws.com\/DEV/g' /usr/share/nginx/html/main*.js
+    sed -i -e 's/https:\/\/28c9y4dvs8.execute-api.us-east-1.amazonaws.com\/CI02/https:\/\/vsfpgmkkvf.execute-api.us-east-1.amazonaws.com\/DEV/g' /usr/share/nginx/html/main*.js
+    sed -i -e 's/authPassword:"12345"/authPassword:"ypej47Qm14QhjB93Sf"/g' /usr/share/nginx/html/main*.js
+
+    echo "Starting Web Server ..."
+    nginx -g 'daemon off;'
+
 elif [ "$environment" == "qa" ]
 then
     echo "Setting up project $app:$environment"
@@ -30,6 +41,17 @@ then
 
     sed -i -e 's/https:\/\/4kz2r4t838.execute-api.us-east-1.amazonaws.com\/CI02/https:\/\/4kz2r4t838.execute-api.us-east-1.amazonaws.com\/QA02/g' /usr/share/nginx/html/main*.js
     sed -i -e 's/https:\/\/28c9y4dvs8.execute-api.us-east-1.amazonaws.com\/CI02/https:\/\/28c9y4dvs8.execute-api.us-east-1.amazonaws.com\/QA02/g' /usr/share/nginx/html/main*.js
+    sed -i -e 's/authPassword:"12345"/authPassword:"ypej47Qm14QhjB93Sf"/g' /usr/share/nginx/html/main*.js
+
+    echo "Starting Web Server ..."
+    nginx -g 'daemon off;'
+
+elif [ "$environment" == "qas" ]
+then
+    echo "Setting up project $app:$environment"
+
+    sed -i -e 's/https:\/\/4kz2r4t838.execute-api.us-east-1.amazonaws.com\/CI02/https:\/\/fduetuxpf0.execute-api.us-east-1.amazonaws.com\/QAS/g' /usr/share/nginx/html/main*.js
+    sed -i -e 's/https:\/\/28c9y4dvs8.execute-api.us-east-1.amazonaws.com\/CI02/https:\/\/vsfpgmkkvf.execute-api.us-east-1.amazonaws.com\/QAS/g' /usr/share/nginx/html/main*.js
     sed -i -e 's/authPassword:"12345"/authPassword:"ypej47Qm14QhjB93Sf"/g' /usr/share/nginx/html/main*.js
 
     echo "Starting Web Server ..."
