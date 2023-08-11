d9cc250c2e60634225fd7a918e56a32d5e71e6f4
Jesus Alejandro Cardenas Vilca <jesus.cardenas@farmaciasperuanas.pe>
2023-08-11 20:45:58 +0000

    Updated setup.sh

--- k8s/setup.sh
+++ k8s/setup.sh
@@ -1,14 +1,11 @@
-#!/bin/sh -e
-echo "Setting up project $app:$environment"
-
-#sed -i -e 's/environment:"develop"/environment:"ci"/g' /usr/share/nginx/html/main*.js
-#sed -i -e 's/lRJsUxGhkBIAV5QNk-5lB7I1e51kod_pQbZm_eu3xaQ/M7z44pJlDaQPRM3__pjFwCunPWuv6suOxsnibluRkRg/g' /usr/share/nginx/html/main*.js
-
-echo "Starting Web Server ..."
-nginx -g 'daemon off;'
-
-if [ "$environment" == "prd" ]
-  echo "Google Tag Manager Settings for PRD enviroment"
-  sed -i -e 's/GTM-NT7T793/GTM-MP4HBG7/g' /usr/share/nginx/html/index.html
+elif [ "$environment" == "uat" ]
 then
+  echo "Google Tag Manager Settings for UAT enviroment"
+  sed -i -e 's/GTM-M8T3RJR/GTM-M8T3RJR/g' /usr/share/nginx/html/index.html
+
+  echo "Starting Web Server Inkafarma UAT"
+  nginx -g 'daemon off;'
+else
+  echo "Starting Web Server"
+  nginx -g 'daemon off;'
 fi
