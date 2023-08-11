151387beb14184f3f81cdac6a2689e70b7904fb7
Jesus Alejandro Cardenas Vilca <jesus.cardenas@farmaciasperuanas.pe>
2023-08-11 20:36:47 +0000

    Updated setup.sh

--- k8s/setup.sh
+++ k8s/setup.sh
@@ -1,75 +1,14 @@
 #!/bin/sh -e
-if [ "$environment" == "ci" ]
+echo "Setting up project $app:$environment"
+
+#sed -i -e 's/environment:"develop"/environment:"ci"/g' /usr/share/nginx/html/main*.js
+#sed -i -e 's/lRJsUxGhkBIAV5QNk-5lB7I1e51kod_pQbZm_eu3xaQ/M7z44pJlDaQPRM3__pjFwCunPWuv6suOxsnibluRkRg/g' /usr/share/nginx/html/main*.js
+
+echo "Starting Web Server ..."
+nginx -g 'daemon off;'
+
+if [ "$environment" == "prd" ]
+  echo "Google Tag Manager Settings for PRD enviroment"
+  sed -i -e 's/GTM-NT7T793/GTM-MP4HBG7/g' /usr/share/nginx/html/index.html
 then
-    echo "Setting up project $app:$environment"
-
-    echo "Starting Web Server ..."
-    nginx -g 'daemon off;'
-
-elif [ "$environment" == "ci2" ]
-then
-    echo "Setting up project $app:$environment"
-
-    echo "Starting Web Server ..."
-    nginx -g 'daemon off;'
-
-elif [ "$environment" == "qa" ]
-then
-    echo "Setting up project $app:$environment"
-
-    sed -i -e 's/https:\/\/4kz2r4t838.execute-api.us-east-1.amazonaws.com\/CI02/https:\/\/4kz2r4t838.execute-api.us-east-1.amazonaws.com\/QA01/g' /usr/share/nginx/html/main*.js
-    sed -i -e 's/https:\/\/28c9y4dvs8.execute-api.us-east-1.amazonaws.com\/CI02/https:\/\/28c9y4dvs8.execute-api.us-east-1.amazonaws.com\/QA01/g' /usr/share/nginx/html/main*.js
-    sed -i -e 's/authPassword:"12345"/authPassword:"ypej47Qm14QhjB93Sf"/g' /usr/share/nginx/html/main*.js
-
-    echo "Starting Web Server ..."
-    nginx -g 'daemon off;'
-
-elif [ "$environment" == "qa2" ]
-then
-    echo "Setting up project $app:$environment"
-
-    sed -i -e 's/https:\/\/4kz2r4t838.execute-api.us-east-1.amazonaws.com\/CI02/https:\/\/4kz2r4t838.execute-api.us-east-1.amazonaws.com\/QA02/g' /usr/share/nginx/html/main*.js
-    sed -i -e 's/https:\/\/28c9y4dvs8.execute-api.us-east-1.amazonaws.com\/CI02/https:\/\/28c9y4dvs8.execute-api.us-east-1.amazonaws.com\/QA02/g' /usr/share/nginx/html/main*.js
-    sed -i -e 's/authPassword:"12345"/authPassword:"ypej47Qm14QhjB93Sf"/g' /usr/share/nginx/html/main*.js
-
-    echo "Starting Web Server ..."
-    nginx -g 'daemon off;'
-
-elif [ "$environment" == "qas-audit" ]
-then
-    echo "Setting up project $app:$environment"
-
-    sed -i -e 's/https:\/\/4kz2r4t838.execute-api.us-east-1.amazonaws.com\/CI02/https:\/\/4kz2r4t838.execute-api.us-east-1.amazonaws.com\/QA02/g' /usr/share/nginx/html/main*.js
-    sed -i -e 's/https:\/\/28c9y4dvs8.execute-api.us-east-1.amazonaws.com\/CI02/https:\/\/28c9y4dvs8.execute-api.us-east-1.amazonaws.com\/QA02/g' /usr/share/nginx/html/main*.js
-    sed -i -e 's/authPassword:"12345"/authPassword:"ypej47Qm14QhjB93Sf"/g' /usr/share/nginx/html/main*.js
-
-    echo "Starting Web Server ..."
-    nginx -g 'daemon off;'
-
-elif [ "$environment" == "uat" ]
-then
-    echo "Setting up project $app:$environment"
-
-    sed -i -e 's/https:\/\/4kz2r4t838.execute-api.us-east-1.amazonaws.com\/CI02/https:\/\/4kz2r4t838.execute-api.us-east-1.amazonaws.com\/UAT/g' /usr/share/nginx/html/main*.js
-    sed -i -e 's/https:\/\/28c9y4dvs8.execute-api.us-east-1.amazonaws.com\/CI02/https:\/\/28c9y4dvs8.execute-api.us-east-1.amazonaws.com\/UAT/g' /usr/share/nginx/html/main*.js
-    sed -i -e 's/authPassword:"12345"/authPassword:"sgrg75498485kojjk54547jjkl"/g' /usr/share/nginx/html/main*.js
-    sed -i -e 's/6ac0e5bb-786a-4371-98f2-5a2e8035d871/e429f7d9-7476-4165-aa13-f4d13b550504/g' /usr/share/nginx/html/main*.js
-
-    echo "Starting Web Server"
-    nginx -g 'daemon off;'
-
-elif [ "$environment" == "prd" ]
-then
-
-    echo "Setting up project $app:$environment"
-
-    sed -i -e 's/https:\/\/4kz2r4t838.execute-api.us-east-1.amazonaws.com\/CI02/https:\/\/4kz2r4t838.execute-api.us-east-1.amazonaws.com\/PRD/g' /usr/share/nginx/html/main*.js
-    sed -i -e 's/https:\/\/28c9y4dvs8.execute-api.us-east-1.amazonaws.com\/CI02/https:\/\/28c9y4dvs8.execute-api.us-east-1.amazonaws.com\/PRD/g' /usr/share/nginx/html/main*.js
-    sed -i -e 's/authPassword:"12345"/authPassword:"jqwPor428Hscx72lhKms"/g' /usr/share/nginx/html/main*.js
-    sed -i -e 's/6ac0e5bb-786a-4371-98f2-5a2e8035d871/e429f7d9-7476-4165-aa13-f4d13b550504/g' /usr/share/nginx/html/main*.js
-
-    echo "Starting Web Server"
-    nginx -g 'daemon off;'
-else
-    echo "Please enter a valid environment option [ ci, ci2, qa, qa2, prd]"
 fi
