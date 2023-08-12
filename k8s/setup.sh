2ba96f658b37723b95d1b6f55585b24f9b0250c7
luisriveradev <luis.rivera@inkafarmadigital.pe>
2023-08-12 11:51:08 -0500

    update server config

--- k8s/setup.sh
+++ k8s/setup.sh
@@ -1,86 +1,19 @@
-#!/bin/sh -e
-if [ "$environment" == "ci" ]
+if [ "$environment" == "prd" ]
 then
-    echo "Setting up project $app:$environment"
+  echo "Google Tag Manager Settings for PRD enviroment"
+  sed -i -e 's/GTM-M8T3RJR/TEST/g' /usr/share/nginx/html/index.html
 
-    echo "Starting Web Server ..."
-    nginx -g 'daemon off;'
-
-elif [ "$environment" == "ci2" ]
-then
-    echo "Setting up project $app:$environment"
-
-    echo "Starting Web Server ..."
-    nginx -g 'daemon off;'
-
-elif [ "$environment" == "dev" ]
-then
-    echo "Setting up project $app:$environment"
-
-    sed -i -e 's/https:\/\/4kz2r4t838.execute-api.us-east-1.amazonaws.com\/CI02/https:\/\/fduetuxpf0.execute-api.us-east-1.amazonaws.com\/DEV/g' /usr/share/nginx/html/main*.js
-    sed -i -e 's/https:\/\/28c9y4dvs8.execute-api.us-east-1.amazonaws.com\/CI02/https:\/\/vsfpgmkkvf.execute-api.us-east-1.amazonaws.com\/DEV/g' /usr/share/nginx/html/main*.js
-    sed -i -e 's/authPassword:"12345"/authPassword:"ypej47Qm14QhjB93Sf"/g' /usr/share/nginx/html/main*.js
-
-    echo "Starting Web Server ..."
-    nginx -g 'daemon off;'
-
-elif [ "$environment" == "qa" ]
-then
-    echo "Setting up project $app:$environment"
-
-    sed -i -e 's/https:\/\/4kz2r4t838.execute-api.us-east-1.amazonaws.com\/CI02/https:\/\/4kz2r4t838.execute-api.us-east-1.amazonaws.com\/QA01/g' /usr/share/nginx/html/main*.js
-    sed -i -e 's/https:\/\/28c9y4dvs8.execute-api.us-east-1.amazonaws.com\/CI02/https:\/\/28c9y4dvs8.execute-api.us-east-1.amazonaws.com\/QA01/g' /usr/share/nginx/html/main*.js
-    sed -i -e 's/authPassword:"12345"/authPassword:"ypej47Qm14QhjB93Sf"/g' /usr/share/nginx/html/main*.js
-
-    echo "Starting Web Server ..."
-    nginx -g 'daemon off;'
-
-elif [ "$environment" == "qa2" ]
-then
-    echo "Setting up project $app:$environment"
-
-    sed -i -e 's/https:\/\/4kz2r4t838.execute-api.us-east-1.amazonaws.com\/CI02/https:\/\/4kz2r4t838.execute-api.us-east-1.amazonaws.com\/QA02/g' /usr/share/nginx/html/main*.js
-    sed -i -e 's/https:\/\/28c9y4dvs8.execute-api.us-east-1.amazonaws.com\/CI02/https:\/\/28c9y4dvs8.execute-api.us-east-1.amazonaws.com\/QA02/g' /usr/share/nginx/html/main*.js
-    sed -i -e 's/authPassword:"12345"/authPassword:"ypej47Qm14QhjB93Sf"/g' /usr/share/nginx/html/main*.js
-
-    echo "Starting Web Server ..."
-    nginx -g 'daemon off;'
-
-elif [ "$environment" == "qas" ]
-then
-    echo "Setting up project $app:$environment"
-
-    sed -i -e 's/https:\/\/4kz2r4t838.execute-api.us-east-1.amazonaws.com\/CI02/https:\/\/fduetuxpf0.execute-api.us-east-1.amazonaws.com\/QAS/g' /usr/share/nginx/html/main*.js
-    sed -i -e 's/https:\/\/28c9y4dvs8.execute-api.us-east-1.amazonaws.com\/CI02/https:\/\/vsfpgmkkvf.execute-api.us-east-1.amazonaws.com\/QAS/g' /usr/share/nginx/html/main*.js
-    sed -i -e 's/authPassword:"12345"/authPassword:"ypej47Qm14QhjB93Sf"/g' /usr/share/nginx/html/main*.js
-
-    echo "Starting Web Server ..."
-    nginx -g 'daemon off;'
-
+  echo "Starting Web Server Inkafarma"
+  nginx -g 'daemon off;'
 elif [ "$environment" == "uat" ]
 then
-    echo "Setting up project $app:$environment"
+  echo "Google Tag Manager Settings for UAT enviroment"
+  sed -i -e 's/GTM-M8T3RJR/GTM-M8T3RJR/g' /usr/share/nginx/html/index.html
 
-    sed -i -e 's/https:\/\/4kz2r4t838.execute-api.us-east-1.amazonaws.com\/CI02/https:\/\/4kz2r4t838.execute-api.us-east-1.amazonaws.com\/UAT/g' /usr/share/nginx/html/main*.js
-    sed -i -e 's/https:\/\/28c9y4dvs8.execute-api.us-east-1.amazonaws.com\/CI02/https:\/\/28c9y4dvs8.execute-api.us-east-1.amazonaws.com\/UAT/g' /usr/share/nginx/html/main*.js
-    sed -i -e 's/authPassword:"12345"/authPassword:"sgrg75498485kojjk54547jjkl"/g' /usr/share/nginx/html/main*.js
-    sed -i -e 's/6ac0e5bb-786a-4371-98f2-5a2e8035d871/e429f7d9-7476-4165-aa13-f4d13b550504/g' /usr/share/nginx/html/main*.js
-
-    echo "Starting Web Server"
-    nginx -g 'daemon off;'
-
-elif [ "$environment" == "prd" ]
-then
-
-    echo "Setting up project $app:$environment"
-
-    sed -i -e 's/https:\/\/4kz2r4t838.execute-api.us-east-1.amazonaws.com\/CI02/https:\/\/4kz2r4t838.execute-api.us-east-1.amazonaws.com\/PRD/g' /usr/share/nginx/html/main*.js
-    sed -i -e 's/https:\/\/28c9y4dvs8.execute-api.us-east-1.amazonaws.com\/CI02/https:\/\/28c9y4dvs8.execute-api.us-east-1.amazonaws.com\/PRD/g' /usr/share/nginx/html/main*.js
-    sed -i -e 's/authPassword:"12345"/authPassword:"jqwPor428Hscx72lhKms"/g' /usr/share/nginx/html/main*.js
-    sed -i -e 's/6ac0e5bb-786a-4371-98f2-5a2e8035d871/e429f7d9-7476-4165-aa13-f4d13b550504/g' /usr/share/nginx/html/main*.js
-
-    echo "Starting Web Server"
-    nginx -g 'daemon off;'
+  echo "Starting Web Server Inkafarma UAT"
+  nginx -g 'daemon off;'
 else
-    echo "Please enter a valid environment option [ ci, ci2, qa, qa2, prd]"
+  echo "Setting up project $app:$environment"
+  echo "Starting Web Server"
+  nginx -g 'daemon off;'
 fi
