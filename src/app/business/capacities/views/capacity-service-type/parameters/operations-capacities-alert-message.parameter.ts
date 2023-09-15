092a09b3c328550f46339b56cc42cce8978da447
= <soluciondigital73@farmaciasperuanas.pe>
2023-09-15 09:44:27 -0500

    updated styles of component capacities module

--- src/app/business/capacities/views/capacity-service-type/parameters/operations-capacities-alert-message.parameter.ts
+++ src/app/business/capacities/views/capacity-service-type/parameters/operations-capacities-alert-message.parameter.ts
@@ -2,6 +2,6 @@ export const capacityAlertSuccessMessage = (
   capacityType: string,
   capacityValue
 ) => {
-  return `<span class="text-body-2-regular text-gray-90">Se realizó la edición de capacidad en el servicio <span class="text-lowercase">${capacityType}</span>
+  return `<span class="text-body-3-regular text-gray-90">Se realizó la edición de capacidad en el servicio <span class="text-lowercase">${capacityType}</span>
           para el local ${capacityValue}</span>`;
 };
